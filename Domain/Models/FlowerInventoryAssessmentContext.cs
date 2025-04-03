2c402662b0f2eaa045bca740b6e123659d1bc3c8
Michalis Papamichael <michalis.papamichael@outlook.com>
2025-04-03 20:56:08 +0300

    Update FlowerInventoryAssessmentContext.cs

--- Domain/Models/FlowerInventoryAssessmentContext.cs
+++ Domain/Models/FlowerInventoryAssessmentContext.cs
@@ -19,9 +19,7 @@ public partial class FlowerInventoryAssessmentContext : DbContext
 
     public virtual DbSet<Flower> Flowers { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=FlowerInventoryAssessment;Trusted_Connection=False;User ID=sa;Password=onlyme1995;TrustServerCertificate=True");
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
