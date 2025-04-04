ad289ea55cc1d9cf41ed73937bdf774ac7469c86
Michalis Papamichael <michalis.papamichael@outlook.com>
2025-04-04 19:45:22 +0300

    Create Repository.cs

---
+++ DataAccess/Repositories/Repository.cs
@@ -0,0 +1,29 @@
+﻿using DataAccess.Repositories.TableRepositories;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class Repository
+    {
+        private readonly FlowerInventoryAssessmentContext _context;
+        public Repository(FlowerInventoryAssessmentContext context)
+        {
+            _context = context;
+            Flowers = new FlowersRepository(_context);
+            Categories = new CategoriesRepository(_context);
+        }
+        public FlowersRepository Flowers { get; private set; }
+        public CategoriesRepository Categories { get; private set; }
+        #region Base methods
+        public async Task SaveChanges()
+        {
+            await _context.SaveChangesAsync();
+        }
+        #endregion
+    }
+}
