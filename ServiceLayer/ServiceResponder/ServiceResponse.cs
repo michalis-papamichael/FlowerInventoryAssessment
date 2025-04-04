5dbba8d8ed42b1f0b106398f14b65bc59d9d0b4a
Michalis Papamichael <michalis.papamichael@outlook.com>
2025-04-04 22:47:13 +0300

    Create ServiceResponse.cs

---
+++ ServiceLayer/ServiceResponder/ServiceResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.ServiceResponder
+{
+    public class ServiceResponse<T>
+    {
+        // Default
+        public ServiceResponse()
+        {
+            ErrorMessages = new List<string>();
+        }
+        public T? Data { get; set; }
+        public bool Success { get; set; }
+        public string? Message { get; set; } = null;
+        public List<string> ErrorMessages { get; set; }
+    }
+}
