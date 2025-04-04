a69d22416e81df38b9699e9649ac43ad4b4bba98
Michalis Papamichael <michalis.papamichael@outlook.com>
2025-04-04 22:47:04 +0300

    Create SCreateFlowerDto.cs

---
+++ ServiceLayer/ServiceDtos/Flowers/SCreateFlowerDto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.ServiceDtos.Flowers
+{
+    public class SCreateFlowerDto
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(150, MinimumLength = 2)]
+        public string Name { get; set; }
+        [StringLength(750)]
+        public string? Description { get; set; }
+        [Precision(9, 2)]
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
