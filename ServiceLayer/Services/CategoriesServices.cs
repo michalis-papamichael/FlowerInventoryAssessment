ee8c19f2ba7eb2515d3c34bccdc7afc193de676f
Michalis Papamichael <michalis.papamichael@outlook.com>
2025-04-04 22:47:16 +0300

    Create CategoriesServices.cs

---
+++ ServiceLayer/Services/CategoriesServices.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Repositories;
+using ServiceLayer.ServiceDtos.Categories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Services
+{
+    public class CategoriesServices
+    {
+        private readonly Repository _context;
+        public CategoriesServices(Repository context)
+        {
+            _context = context;
+        }
+    }
+}
