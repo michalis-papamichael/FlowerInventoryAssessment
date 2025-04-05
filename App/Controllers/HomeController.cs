e1d90f140d523b94047768c84cf24679f8bbfae4
Michalis Papamichael <michalis.papamichael@outlook.com>
2025-04-05 15:28:45 +0300

    removed unused views (privacy)

--- App/Controllers/HomeController.cs
+++ App/Controllers/HomeController.cs
@@ -1,24 +1,19 @@
 using System.Diagnostics;
 using App.Models;
 using Microsoft.AspNetCore.Mvc;
+using ServiceLayer.Services;
 
 namespace App.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
