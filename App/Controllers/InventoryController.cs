9ba221b4dd8e953f536d4261459a071254c88dfa
Michalis Papamichael <michalis.papamichael@outlook.com>
2025-04-05 18:25:24 +0300

    GetFlowers datatable action

--- App/Controllers/InventoryController.cs
+++ App/Controllers/InventoryController.cs
@@ -20,4 +20,65 @@ namespace App.Controllers
             ViewData["Layout"] = "_InventoryLayout";
             return View();
         }
+        // used for datatable calls
+        [HttpPost]
+        public async Task<IActionResult> GetFlowers()
+        {
+            DatatableRequestModel dtModel = DatatablesHelper.ConstructModel(Request);
+            ServiceResponse<SFlowersPagingDto> response = await _flowersServices.GetFlowersWithPaging(dtModel.Skip, dtModel.PageSize);
+            if (response.Success && response.Data != null)
+            {
+                var dto = response.Data.Flowers;
+                if (dto.Count > 0)
+                {
+                    // sorting
+                    //if (dtModel.CanSort)
+                    //{
+                    //    if (dtModel.IsAsc)
+                    //    {
+                    //    }
+                    //    else
+                    //    {
+                    //    }
+                    //}
+
+                    // searching
+                    if (!string.IsNullOrEmpty(dtModel.SearchValue))
+                    {
+                        string searchValue = dtModel.SearchValue.ToLower();
+                        dto = dto.Where(x => x.Name.ToLower().Contains(searchValue)
+                        || x.CategoryName.ToLower().Contains(searchValue)
+                        || x.Price.ToString().ToLower().Contains(searchValue)).ToList();
+                    }
+                    int totalrecords = response.Data.TotalFlowers;
+                    var json = new
+                    {
+                        draw = dtModel.Draw,
+                        recordsFiltered = totalrecords,
+                        recordsTotal = totalrecords,
+                        data = dto,
+                    };
+                    return Ok(json);
+                }
+
+            }
+            else
+            {
+                if (response.Exception != null)
+                {
+                    Log.Warning(response.Exception, response?.Message ?? "");
+                }
+                else
+                {
+                    Log.Warning(response?.Message ?? "");
+                }
+            }
+            var errorjson = new
+            {
+                draw = dtModel.Draw,
+                recordsTotal = 0,
+            };
+            return Ok(errorjson);
+        }
+    }
 }
