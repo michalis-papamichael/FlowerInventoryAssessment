a365e6d784dfd07b523cd10d768780d0c266aba5
Michalis Papamichael <michalis.papamichael@outlook.com>
2025-04-05 12:41:48 +0300

    Main unit test class
    With unit test for checking flowers repo

---
+++ Tests/MainUnitTest.cs
@@ -0,0 +1,45 @@
+using App;
+using DataAccess.Repositories;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tests
+{
+    public class MainUnitTest : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        public MainUnitTest()
+        {
+            WebApplicationFactory<Program> factory = new WebApplicationFactory<Program>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.UseSetting("IsTesting", "true");
+                });
+            _factory = factory;
+        }
+        [Fact]
+        public async Task GetFlowers_Should_Return_MoreThanZero()
+        {
+            try
+            {
+                //Arrange
+                var serviceProvider = _factory.Services.GetRequiredService<IServiceProvider>();
+                using (var scope = serviceProvider.CreateScope())
+                {
+                    var _context = scope.ServiceProvider.GetRequiredService<Repository>();
+
+                    //Acts
+                    List<Flower> flower = await _context.Flowers.GetFlowersWithPagingAsync(0, 10);
+                    int total = flower.Count();
+
+                    //Assert
+                    Assert.NotEqual(0, total);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
