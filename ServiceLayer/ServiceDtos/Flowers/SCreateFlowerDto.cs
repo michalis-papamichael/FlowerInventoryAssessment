556c97c97794ca5a6552e5ee2973189ad63da91c
Michalis Papamichael <michalis.papamichael@outlook.com>
2025-04-05 22:23:17 +0300

    data annotations in service create flower dto

--- ServiceLayer/ServiceDtos/Flowers/SCreateFlowerDto.cs
+++ ServiceLayer/ServiceDtos/Flowers/SCreateFlowerDto.cs
@@ -10,13 +10,13 @@ namespace ServiceLayer.ServiceDtos.Flowers
 {
     public class SCreateFlowerDto
     {
-        [Key]
-        public int Id { get; set; }
+        [Required]
         [StringLength(150, MinimumLength = 2)]
         public string Name { get; set; }
         [StringLength(750)]
         public string? Description { get; set; }
         [Precision(9, 2)]
+        [Range(0, double.MaxValue)]
         public decimal Price { get; set; }
         public int TotalInventory { get; set; }
         public int CategoryId { get; set; }
