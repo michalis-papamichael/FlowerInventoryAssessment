a6c65e221d61b5d5200d8212d39caea46dc4082e
Michalis Papamichael <michalis.papamichael@outlook.com>
2025-04-05 17:48:34 +0300

    Create DatatablesHelper.cs

---
+++ App/Helpers/DatatablesHelper.cs
@@ -0,0 +1,26 @@
+﻿using App.Models;
+
+namespace App.Helpers
+{
+    public static class DatatablesHelper
+    {
+        public static DatatableRequestModel ConstructModel(HttpRequest request)
+        {
+            DatatableRequestModel datatablesRequest = new DatatableRequestModel();
+
+            datatablesRequest.Draw = request.Form["draw"].FirstOrDefault();
+            datatablesRequest.Start = request.Form["start"].FirstOrDefault();
+            datatablesRequest.Length = request.Form["length"].FirstOrDefault();
+
+            datatablesRequest.SortColIndex = Convert.ToInt32(request.Form["order[0][column]"]);
+
+            datatablesRequest.SortColumnDirection = request.Form["order[0][dir]"];
+
+            datatablesRequest.SearchValue = request.Form["search[value]"].FirstOrDefault();
+            datatablesRequest.PageSize = datatablesRequest.Length != null ? Convert.ToInt32(datatablesRequest.Length) : 0;
+            datatablesRequest.Skip = datatablesRequest.Start != null ? Convert.ToInt32(datatablesRequest.Start) : 0;
+
+            return datatablesRequest;
+        }
+    }
+}
