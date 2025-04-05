c1f1a32b9cdc2a220c21698551a41753a7a2b2b1
Michalis Papamichael <michalis.papamichael@outlook.com>
2025-04-05 17:51:13 +0300

    changing layout on runtime with viewdata

---
+++ App/Controllers/InventoryController.cs
@@ -0,0 +1,23 @@
+﻿using App.Helpers;
+using App.Models;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using ServiceLayer.ServiceDtos.Flowers;
+using ServiceLayer.ServiceResponder;
+using ServiceLayer.Services;
+
+namespace App.Controllers
+{
+    public class InventoryController : Controller
+    {
+        private readonly FlowersServices _flowersServices;
+        public InventoryController(FlowersServices flowersServices)
+        {
+            _flowersServices = flowersServices;
+        }
+        public IActionResult Details()
+        {
+            ViewData["Layout"] = "_InventoryLayout";
+            return View();
+        }
+}
