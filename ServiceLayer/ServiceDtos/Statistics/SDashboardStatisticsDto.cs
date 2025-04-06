06ed4179d5e429b21bac791e61b040b5f6e44667
Michalis Papamichael <michalis.papamichael@outlook.com>
2025-04-06 19:16:38 +0300

    Create SDashboardStatisticsDto.cs

---
+++ ServiceLayer/ServiceDtos/Statistics/SDashboardStatisticsDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.ServiceDtos.Statistics
+{
+    public class SDashboardStatisticsDto
+    {
+        public int TotalFlowersInInventory { get; set; }
+        public int TotalKindsOfFlowers { get; set; }
+    }
+}
