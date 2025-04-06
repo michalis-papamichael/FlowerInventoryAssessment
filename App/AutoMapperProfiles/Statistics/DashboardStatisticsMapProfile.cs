e7c54f0fbbbd6aed79b52d97884341c74b53c128
Michalis Papamichael <michalis.papamichael@outlook.com>
2025-04-06 19:17:07 +0300

    Create DashboardStatisticsMapProfile.cs

---
+++ App/AutoMapperProfiles/Statistics/DashboardStatisticsMapProfile.cs
@@ -0,0 +1,14 @@
+﻿using App.Dtos.Statistics;
+using AutoMapper;
+using ServiceLayer.ServiceDtos.Statistics;
+
+namespace App.AutoMapperProfiles.Statistics
+{
+    public class DashboardStatisticsMapProfile : Profile
+    {
+        public DashboardStatisticsMapProfile()
+        {
+            CreateMap<SDashboardStatisticsDto, DashboardStatisticsDto>();
+        }
+    }
+}
