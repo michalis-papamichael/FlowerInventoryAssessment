18fff134b0b48d522b7793ac408cd397185f3590
Michalis Papamichael <michalis.papamichael@outlook.com>
2025-04-06 14:54:50 +0300

    added global sweetalert2 toast to be called whenever we need it

--- App/wwwroot/js/site.js
+++ App/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// global sweetalert toast
+const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+    }
+});
