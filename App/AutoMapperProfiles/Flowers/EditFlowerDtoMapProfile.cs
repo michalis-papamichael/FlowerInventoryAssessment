f006d9466af930d57450918a2f47055c0e6d052e
Michalis Papamichael <michalis.papamichael@outlook.com>
2025-04-06 12:43:30 +0300

    Create EditFlowerDtoMapProfile.cs

---
+++ App/AutoMapperProfiles/Flowers/EditFlowerDtoMapProfile.cs
@@ -0,0 +1,15 @@
+﻿using App.Dtos.Flowers;
+using AutoMapper;
+using ServiceLayer.ServiceDtos.Flowers;
+
+namespace App.AutoMapperProfiles.Flowers
+{
+    public class EditFlowerDtoMapProfile : Profile
+    {
+        public EditFlowerDtoMapProfile()
+        {
+            CreateMap<SEditFlowerDto, EditFlowerDto>();
+            CreateMap<EditFlowerDto, SEditFlowerDto>();
+        }
+    }
+}
