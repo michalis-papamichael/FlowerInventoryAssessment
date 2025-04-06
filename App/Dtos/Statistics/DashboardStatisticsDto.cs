939136b6c055048a98de871c71ed830fc52f4e2a
Michalis Papamichael <michalis.papamichael@outlook.com>
2025-04-06 19:17:05 +0300

    Create DashboardStatisticsDto.cs

---
+++ App/Dtos/Statistics/DashboardStatisticsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace App.Dtos.Statistics
+{
+    public class DashboardStatisticsDto
+    {
+        public int TotalFlowersInInventory { get; set; }
+        public int TotalKindsOfFlowers { get; set; }
+        public bool ShoStatistics { get; set; }
+    }
+}
