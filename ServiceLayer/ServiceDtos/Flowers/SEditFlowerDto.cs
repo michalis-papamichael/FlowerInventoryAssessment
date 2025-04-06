9f2a9a47096339757fab939754ad9fe2a91eb215
Michalis Papamichael <michalis.papamichael@outlook.com>
2025-04-06 12:43:18 +0300

    Create SEditFlowerDto.cs

---
+++ ServiceLayer/ServiceDtos/Flowers/SEditFlowerDto.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.ServiceDtos.Flowers
+{
+    public class SEditFlowerDto
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(150, MinimumLength = 2)]
+        public string Name { get; set; }
+        [StringLength(750)]
+        public string? Description { get; set; }
+        [Precision(9, 2)]
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+        public int TotalInventory { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
