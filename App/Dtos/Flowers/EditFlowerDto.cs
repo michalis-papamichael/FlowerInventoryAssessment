f8a4faf9162e030b641857e752f613decd8a0a19
Michalis Papamichael <michalis.papamichael@outlook.com>
2025-04-06 12:43:20 +0300

    Create EditFlowerDto.cs

---
+++ App/Dtos/Flowers/EditFlowerDto.cs
@@ -0,0 +1,33 @@
+﻿using App.Dtos.Categories;
+using App.Models;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace App.Dtos.Flowers
+{
+    public class EditFlowerDto
+    {
+        public EditFlowerDto()
+        {
+            Categories = new List<CategoryDto>();
+            Messages = new List<ViewMessage>();
+        }
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(150, MinimumLength = 2)]
+        public string Name { get; set; }
+        [StringLength(750)]
+        public string? Description { get; set; }
+        [Precision(9, 2)]
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+        public int TotalInventory { get; set; }
+        public int CategoryId { get; set; }
+
+        // Categories for the select
+        public List<CategoryDto> Categories { get; set; }
+        // Used to show message to user according to outcome
+        public List<ViewMessage> Messages { get; set; }
+    }
+}
