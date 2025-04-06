86b729156594db2c00c2a41fc64688eddc65769c
Michalis Papamichael <michalis.papamichael@outlook.com>
2025-04-06 23:18:20 +0300

    Create AppPaths.cs
    static paths

---
+++ App/Statics/AppPaths.cs
@@ -0,0 +1,11 @@
+﻿namespace App.Statics
+{
+    public static class AppPaths
+    {
+        //where images will be stored
+        public static string FLOWERS_PHYSICAL_STORAGE = "C:\\flowers";
+
+        // in which path static images will be called
+        public static string STATIC_FILES = "/StaticFiles";
+    }
+}
