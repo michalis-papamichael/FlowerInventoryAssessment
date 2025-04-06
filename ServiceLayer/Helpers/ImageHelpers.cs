344757b966ccdb2a37c7f0af7e435df29932b0eb
Michalis Papamichael <michalis.papamichael@outlook.com>
2025-04-06 23:17:55 +0300

    helper to try store image

---
+++ ServiceLayer/Helpers/ImageHelpers.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using ServiceLayer.ServiceDtos.Flowers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Helpers
+{
+    internal static class ImageHelpers
+    {
+        internal static async Task<string?> TryStoreImage(IFormFile? file, string? physicalPath)
+        {
+            string? imageUri = null;
+            if (file != null && physicalPath != null)
+            {
+                var filePath = physicalPath + "\\" + file.FileName;
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                    imageUri = file.FileName;
+                }
+            }
+
+            return imageUri;
+        }
+    }
+}
